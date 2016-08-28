33beac759c1f63b39857f683e26b8b691693928e
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2016-08-27 17:55:40 -0700

    BREAKING CHANGE; Update result output to be more type-checkable.

--- swat.js
+++ swat.js
@@ -20,20 +20,41 @@ const asPromise = fn => (...args) => args.length + 1 === fn.length
   ? new Promise(done => fn(...args, done))
   : Promise.resolve(fn(...args))
 
+const cons = (item, arr) => [item].concat(arr);
+
 // LIB
 
+const TEST = 'TEST';
+const SUITE = 'SUITE';
+const PASS = 'pass';
+const FAIL = 'fail';
+
 const hooks = ['before', 'beforeEach', 'afterEach', 'after'];
+
+const injectName = name => obj => Object.assign({}, obj, { name });
+const injectDuration = (start, now) => obj => Object.assign({}, obj, { duration: now() - start });
+
+const createPass = () => ({ type: TEST, result: PASS });
+const createFail = error => ({ type: TEST, result: FAIL, error });
 
 const runTest = now => context => test => {
   let r;
   const t = now();
   try { r = asPromise(test)(context)
-    .then(result => result === true
-      ? { result: 'pass', timeElapsed: now() - t }
-      : { result: 'fail', error: result, timeElapsed: now() - t }
-    )
-  } catch(error) { r = Promise.resolve({ result: 'fail', error, timeElapsed: now() - t });}
-  return r;
+    .then(result => result === true ? createPass() : createFail(result))
+  } catch(error) { r = Promise.resolve(createFail(error)); }
+  return r.then(injectDuration(t, now));
+}
+
+const addResult = previousResults => result => {
+  switch (result.type) {
+    case TEST:
+      return Object.assign(previousResults, { tests: cons(result, previousResults.tests) });
+    case SUITE:
+      return Object.assign(previousResults, { suites: cons(result, previousResults.suites) });
+    default:
+      throw Error("Trying to add a result of neither TEST nor SUITE type " + JSON.stringify(result, null, 2))
+  }
 }
 
 const runFull = now => (prevBeforeEaches, prevAfterEaches) => testObj => {
@@ -47,20 +68,25 @@ const runFull = now => (prevBeforeEaches, prevAfterEaches) => testObj => {
     .filter(key => !hooks.find(hook => hook === key))
     .map(key => [key, testObj[key]])
   return Promise.resolve(testObj.before ? asPromise(testObj.before)() : void(0))
-    .then(() => asyncReduce(testAndSuiteTuples, {}, (prev, [name, tos]) => (typeof tos === 'function'
-      ? asyncReduce(beforeEaches, void(0), (prev, be) => asPromise(be)(prev))
-        .then(context => runTest(now)(context)(tos)
-          .then(returnPrevious(() =>
-            asyncReduce(afterEaches, context, (prev, ae) => asPromise(ae)(prev))
-          ))
+    .then(() => asyncReduce(
+      testAndSuiteTuples,
+      { type: SUITE, tests: [], suites: [] },
+      (prev, [name, tos]) =>
+        ( typeof tos === 'function'
+        ? asyncReduce(beforeEaches, void(0), (prev, be) => asPromise(be)(prev))
+          .then(context => runTest(now)(context)(tos)
+            .then(returnPrevious(() =>
+              asyncReduce(afterEaches, context, (prev, ae) => asPromise(ae)(prev))
+            ))
+          )
+        : typeof tos === 'object'
+          ? runFull(now)(beforeEaches, afterEaches)(tos)
+          : Promise.resolve(
+            createFail('All test object values must be a function (test) or an object (suite)')
+          )
         )
-      : typeof tos === 'object'
-        ? runFull(now)(beforeEaches, afterEaches)(tos)
-        : Promise.resolve({
-            result: 'fail',
-            error: 'All test object values must be a function (test) or an object (suite)',
-          })
-      ).then(result => Object.assign({}, prev, {[name]: result}))
+        .then(injectName(name))
+        .then(addResult(prev))
     ).then(returnPrevious(() => testObj.after
       ? asPromise(testObj.after)()
       : Promise.resolve(void(0)))
@@ -80,4 +106,8 @@ module.exports = {
   runFull,
   run,
   assertMany,
+  TEST,
+  SUITE,
+  PASS,
+  FAIL,
 }
